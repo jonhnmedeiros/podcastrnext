c2fe016a8c3eac6ec2cfce1bcf8580fdd4bb1306
Jonathan Felipe Medeiros <jonathan.info@gmail.com>
2021-04-21 09:06:14 -0300

    first commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,21 @@
+import { GetStaticProps } from 'next';
+
+export default function Home(props) {
+
+  return (
+    <h1>Index</h1>
+  )
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+  const response = await fetch('http://localhost:3333/episodes')
+  const data = await response.json()
+
+  return { 
+    props: {
+      episodes: data,
+    },
+    revalidate: 60 * 60 * 8,
+  
+  }
+}
